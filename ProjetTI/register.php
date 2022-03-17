87e0d58fde591bd42a2a1c914b1c3f8d3812c062
Junior45i <pascal.vanmalderen@hotmail.be>
2022-03-17 09:57:17 +0100

    test

--- ProjetTI/register.php
+++ ProjetTI/register.php
@@ -15,8 +15,8 @@ try{
         // if(!empty($_POST['nom'])&&!empty($_POST['prenom'])&&!empty($_POST['naissance'])&&!empty($_POST['section'])&&!empty($_POST['mail'])&&!empty($_POST['mdp']))
 
         $sql=$conn->prepare("INSERT INTO membre (nomMem, preMem, dateNmembre, section, mail, mdpMembre) 
-                VALUES ('test', 'Pascal', '2021-02-10', 'IG', 'Test@gmail.com', 'Test1234');");
-        // $sql->bindParam(':nom', $nom, PDO::PARAM_STR, 50);
+                VALUES (:nom, 'Pascal', '2021-02-10', 'IG', 'Test@gmail.com', 'Test1234');");
+        $sql->bindParam(':nom', $nom, PDO::PARAM_STR, 50);
         // $sql->bindParam(':prenom', $prenom, PDO::PARAM_STR, 50);
         // $sql->bindParam(':naissance', $naissance, PDO::PARAM_STR, 50);
         // $sql->bindParam(':section', $section, PDO::PARAM_STR, 50);
