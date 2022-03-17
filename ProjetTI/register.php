cbf3f6854ffaf77288e89844b93dc2c8b47f3ba3
Junior45i <pascal.vanmalderen@hotmail.be>
2022-03-17 19:23:51 +0100

    ajout du register

--- ProjetTI/register.php
+++ ProjetTI/register.php
@@ -3,29 +3,29 @@
 include 'config/database.php';
 $conn = connectDB("localhost", "proj_tm_bdd", "root", "");
 // Action du formulaire
+
 try{
     if(isset($_POST['register'])){
         $nom = $_POST['nom']; 
         $prenom = $_POST['prenom']; 
-        $naissance = $_POST['naissance']; 
+        $naissance = $_POST['naissance'];
         $section = $_POST['section']; 
         $mail = $_POST['mail']; 
         $mdp = $_POST['mdp']; 
         // Vérification de 100% des champs remplit
-        // if(!empty($_POST['nom'])&&!empty($_POST['prenom'])&&!empty($_POST['naissance'])&&!empty($_POST['section'])&&!empty($_POST['mail'])&&!empty($_POST['mdp']))
+        if(!empty($nom)&&!empty($prenom)&&!empty($naissance)&&!empty($section)&&!empty($mail)&&!empty($mdp)){
 
-        echo $nom;
-        $sql=$conn->prepare("INSERT INTO membre (nomMem, preMem, dateNmembre, section, mail, mdpMembre) 
-                VALUES (:nom, 'Pascal', '2021-02-10', 'IG', 'Test@gmail.com', 'Test1234');");
+        $sql=$conn->prepare("INSERT INTO membre (nomMem, preMem, dateNmembre, section, mail, mdpMembre, idMem) 
+                VALUES (:nom, :prenom, :naissance, :section, :mail, :mdp, NULL);");
         $sql->bindParam(':nom', $nom, PDO::PARAM_STR, 50);
-        // $sql->bindParam(':prenom', $prenom, PDO::PARAM_STR, 50);
-        // $sql->bindParam(':naissance', $naissance, PDO::PARAM_STR, 50);
-        // $sql->bindParam(':section', $section, PDO::PARAM_STR, 50);
-        // $sql->bindParam(':mail', $mail, PDO::PARAM_STR, 50);
-        // $sql->bindParam(':mdp', $mdp, PDO::PARAM_STR, 50);
+        $sql->bindParam(':prenom', $prenom, PDO::PARAM_STR, 50);
+        $sql->bindParam(':naissance', $naissance, PDO::PARAM_STR, 50);
+        $sql->bindParam(':section', $section, PDO::PARAM_STR, 50);
+        $sql->bindParam(':mail', $mail, PDO::PARAM_STR, 50);
+        $sql->bindParam(':mdp', $mdp, PDO::PARAM_STR, 50);
         $sql->execute();
 
-
+        }
         // Ajouter vérification mdp (chapitre 7)
 
         //Vérifier adresse email valide (chapitre 7)
