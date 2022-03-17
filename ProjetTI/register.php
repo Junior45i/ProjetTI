116d47051b061ff93f3221994a738432ca6ffdb6
Junior45i <pascal.vanmalderen@hotmail.be>
2022-03-17 09:39:02 +0100

    test

--- ProjetTI/register.php
+++ ProjetTI/register.php
@@ -15,13 +15,13 @@ try{
         // if(!empty($_POST['nom'])&&!empty($_POST['prenom'])&&!empty($_POST['naissance'])&&!empty($_POST['section'])&&!empty($_POST['mail'])&&!empty($_POST['mdp']))
 
         $sql=$conn->prepare("INSERT INTO membre (nomMem, preMem, dateNmembre, section, mail, mdpMembre) 
-                VALUES (:nom, :prenom, :naissance, :section, :mail, :mdp);");
+                VALUES (:nom, 'Pascal', '2021-02-10', 'IG', 'Test@gmail.com', 'Test1234');");
         $sql->bindParam(':nom', $nom, PDO::PARAM_STR, 50);
-        $sql->bindParam(':prenom', $prenom, PDO::PARAM_STR, 50);
-        $sql->bindParam(':naissance', $naissance, PDO::PARAM_STR, 50);
-        $sql->bindParam(':section', $section, PDO::PARAM_STR, 50);
-        $sql->bindParam(':mail', $mail, PDO::PARAM_STR, 50);
-        $sql->bindParam(':mdp', $mdp, PDO::PARAM_STR, 50);
+        // $sql->bindParam(':prenom', $prenom, PDO::PARAM_STR, 50);
+        // $sql->bindParam(':naissance', $naissance, PDO::PARAM_STR, 50);
+        // $sql->bindParam(':section', $section, PDO::PARAM_STR, 50);
+        // $sql->bindParam(':mail', $mail, PDO::PARAM_STR, 50);
+        // $sql->bindParam(':mdp', $mdp, PDO::PARAM_STR, 50);
         $sql->execute();
 
 
