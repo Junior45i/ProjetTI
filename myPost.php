fd9e6846ce54733096146732b1631d1b7277176f
Junior45i <pascal.vanmalderen@hotmail.be>
2022-05-08 21:52:17 +0200

    changement commentaire

--- myPost.php
+++ myPost.php
@@ -6,7 +6,7 @@ include('includes/fonctions.php');
 
 
 //Récupère toutes les questions
-$getAllMyQuestions = $conn->prepare('SELECT title, content, idPubli FROM publication WHERE idMem=:idMem ORDER BY idPubli DESC');
+$getAllMyQuestions = $conn->prepare('SELECT title, content, idPubli FROM publication WHERE idMem=:idMem ORDER BY idPubli ASC');
 $getAllMyQuestions->bindParam(':idMem', $_SESSION['user_id'], PDO::PARAM_STR, 50);
 
 $getAllMyQuestions->execute();
