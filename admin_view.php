c878a7f1923802ba5284bd5adbaeaf0b80c4c0cf
Junior45i <pascal.vanmalderen@hotmail.be>
2022-05-09 14:57:20 +0200

    Ajout de la checkbox

--- admin_view.php
+++ admin_view.php
@@ -33,7 +33,7 @@ include('includes/fonctions.php');
                                         <td>
                                             <div class="form-check form-switch">
                                                 <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
-                                                <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+                                                <label class="form-check-label" for="flexSwitchCheckChecked"></label>
                                             </div>
                                         </td>
                                         <td>
