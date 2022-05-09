6960ad4b5cf5ad94091f97a002000dedce944c85
Junior45i <pascal.vanmalderen@hotmail.be>
2022-05-09 20:58:08 +0200

    vérif de si admin

--- filters/admin_filter.php
+++ filters/admin_filter.php
@@ -1,7 +1,7 @@
 <?php
 // Verifie si une variable de session existe sinon redirige vers la connection
 
-if(!isset($_SESSION['administrateur']) && !isset($_SESSION['administrateur'])){
+if($_SESSION['administrateur']!=1 || empty($_SESSION['administrateur'])){
     header('Location: feed');
     exit();
 }
